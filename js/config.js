2066158287235965872f84a5882f292813278ab2
car1qqtest <109641515+car1qqtest@users.noreply.github.com>
2022-07-20 11:24:44 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBpsBdBbaUvAe04KywoN2caAPf9T69_IGQ",
-    authDomain: "drive-clone-cb216.firebaseapp.com",
-    projectId: "drive-clone-cb216",
-    storageBucket: "drive-clone-cb216.appspot.com",
-    messagingSenderId: "1098261564496",
-    appId: "1:1098261564496:web:1d76ea6f495a8b63295555",
-    measurementId: "G-7SB9S4NDNG"
+    apiKey: "AIzaSyA3JepHPMzzYus_bVFHXXGePa4BdZGLF4Q",
+  authDomain: "drive-clone-98fc4.firebaseapp.com",
+  databaseURL: "https://drive-clone-98fc4-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "drive-clone-98fc4",
+  storageBucket: "drive-clone-98fc4.appspot.com",
+  messagingSenderId: "75340928536",
+  appId: "1:75340928536:web:13157ce14962dd65ff156d",
+  measurementId: "G-Q18369SW0V"
   };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
